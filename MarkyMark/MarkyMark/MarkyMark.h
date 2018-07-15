0f2f5a5210b09394691b17a5e2f409612fad8459
Jonathan Landon <jlandon@me.com>
2018-07-14 20:52:10 -0500

    fix Xcode project

---
+++ MarkyMark/MarkyMark/MarkyMark.h
@@ -0,0 +1,19 @@
+//
+//  MarkyMark.h
+//  MarkyMark
+//
+//  Created by Jonathan Landon on 7/14/18.
+//  Copyright © 2018 Jonathan Landon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MarkyMark.
+FOUNDATION_EXPORT double MarkyMarkVersionNumber;
+
+//! Project version string for MarkyMark.
+FOUNDATION_EXPORT const unsigned char MarkyMarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarkyMark/PublicHeader.h>
+
+
